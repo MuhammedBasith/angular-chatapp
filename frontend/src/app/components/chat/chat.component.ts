06b2d7287116ef47b06dc5d3a19ad2f2f2d78fcc
MuhammedBasith <iambasith123@gmail.com>
2024-12-31 07:04:00 +0530

    add types

--- frontend/src/app/components/chat/chat.component.ts
+++ frontend/src/app/components/chat/chat.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../../services/chat.service';
+import { ServerResponse } from '../../types/ws.types';
 
 @Component({
   selector: 'app-chat',
@@ -8,12 +9,15 @@ import { ChatService } from '../../services/chat.service';
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
 
   public message: string = ''
   public messages: string[] = []
 
   constructor(private chatService: ChatService) {}
+  ngOnInit(): void {
+    this.listMessage()
+  }
 
   public sendMessage(){
     this.messages.push(this.message)
@@ -22,7 +26,10 @@ export class ChatComponent {
   }
 
   public listMessage(){
-    
+    this.chatService.listMessage().subscribe((data: ServerResponse) => {
+      console.log(data);
+      this.messages.push(data.data)
+    })
   }
 
 }
